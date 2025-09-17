5bda225de15f60d454373c06a04f6293e6215883
bignovelist <43513665+bignovelist@users.noreply.github.com>
2025-09-17 22:47:16 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -133,6 +133,7 @@ $(document).ready(function() {
 
 	// Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
+	var carousels = bulmaCarouse2.attach('.carousel', options);
 	
     bulmaSlider.attach();
     
